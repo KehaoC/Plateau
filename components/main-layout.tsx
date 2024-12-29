5a5d537f7283e583fe4f20537b9e99b99d412265
KehaoC <c13576076725@gmail.com>
2024-12-29 18:47:52 +0800

    [bug] 部署时的页面布局 bug

---
+++ components/main-layout.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import { MainContent } from "@/components/main-content"
+import { ViewType } from "@/lib/types"
+import { useState } from "react"
+import { Sidebar } from "@/components/sidebar"
+import { TopBar } from "@/components/top-bar"
+import { MusicPlayer } from "@/components/music-player"
+
+export function MainLayout() {
+    const [activeView, setActiveView] = useState<ViewType>('music-generation')
+
+    return (
+        <div className="h-screen flex flex-col">
+            <div className="flex-1 flex overflow-hidden">
+                <Sidebar setActiveView={setActiveView} activeView={activeView} />
+                <div className="flex-1 flex flex-col">
+                    <TopBar />
+                    <div className="flex-1 overflow-auto">
+                        <MainContent activeView={activeView} />
+                    </div>
+                </div>
+            </div>
+            <MusicPlayer />
+        </div>
+    )
+} 
